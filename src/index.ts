4cfbe710a3ee2114415a1f948eb5629b3137e8aa
Tymoteusz <tymoteusz.telega@euvic.pl>
2021-03-04 14:47:52 +0100

    Added interval

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Client } from 'discord.js';
 import { onMessage, onReady } from './events';
 import { config } from './config';
+import axios from 'axios';
 
 const client = new Client();
 client.login(config.BOT_ID);
 
 client.on('ready', () => onReady(client));
 client.on('message', onMessage);
+
+/* KEEP GLITCH ALIVE */
+setInterval(async () => await axios.get('https://rough-alkaline-slope.glitch.me').then(() => 'Pinged!'), 4 * 60 * 1000);
